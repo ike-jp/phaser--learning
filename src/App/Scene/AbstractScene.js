ab5990ea6569c73869c92b406cc2c752d756b8ff
ike-jp <ike-jp@outlook.com>
2016-03-20 15:56:43 +0900

    シーンの抽象クラス追加

---
+++ src/App/Scene/AbstractScene.js
@@ -0,0 +1,44 @@
+
+/**
+ * シーンの抽象クラス
+ *
+ * @param {Phaser.Game} game
+ * @constructor
+ */
+AbstractScene = function(game)
+{
+	// have nothing to do
+}
+
+/**
+ * このシーン開始前に必要なリソースをロードする処理
+ */
+AbstractScene.prototype.preload = function()
+{
+	// have nothing to do
+}
+
+/**
+ * このシーンの初期化処理
+ */
+AbstractScene.prototype.create = function()
+{
+	// have nothing to do
+}
+
+/**
+ * このシーンの更新処理
+ */
+AbstractScene.prototype.update = function()
+{
+	// have nothing to do
+}
+
+/**
+ * このシーンの描画処理
+ */
+AbstractScene.prototype.render = function()
+{
+	// have nothing to do
+}
+
