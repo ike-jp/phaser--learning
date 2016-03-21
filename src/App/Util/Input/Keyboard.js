79712ca74309d61de5cf77bc9fff34b7b144e197
ike-jp <ike-jp@outlook.com>
2016-03-21 19:18:21 +0900

    キーボードクラスのアノテーション追加

--- src/App/Util/Input/Keyboard.js
+++ src/App/Util/Input/Keyboard.js
@@ -17,7 +17,7 @@ App.Util.Input.Keyboard = function(keyboard)
 	this.keyboard_ = keyboard;
 	this.state_ = [];
 	this.dobule_buffer_idx_ = 0;
-};
+}
 
 /**
  * このキーボードの状態を更新する
@@ -28,12 +28,13 @@ App.Util.Input.Keyboard.prototype.update = function()
 	this.state_.forEach(function(val, key_code) {
 		val[this.dobule_buffer_idx_] = this.keyboard_.isDown(key_code);
 	}, this);
-};
+}
 
 /**
  * キーコードで指定されたキーの押された瞬間をチェックする
  *
  * @param {Phaser.KeyCode} key_code
+ * @param {bool} キーが押され始めたフレームのみtrueを返す
  */
 App.Util.Input.Keyboard.prototype.isTriggered = function(key_code)
 {
@@ -41,12 +42,13 @@ App.Util.Input.Keyboard.prototype.isTriggered = function(key_code)
 	var idx = this.dobule_buffer_idx_;
 	return this.state_[key_code][idx]
 		&& !this.state_[key_code][idx ^1];
-};
+}
 
 /**
  * キーコードで指定されたキーの離された瞬間をチェックする
  *
  * @param {Phaser.KeyCode} key_code
+ * @param {bool} キーが離されたフレームのみtrueを返す
  */
 App.Util.Input.Keyboard.prototype.isReleased = function(key_code)
 {
@@ -54,14 +56,13 @@ App.Util.Input.Keyboard.prototype.isReleased = function(key_code)
 	var idx = this.dobule_buffer_idx_;
 	return !this.state_[key_code][idx]
 		&& this.state_[key_code][idx ^1];
-};
+}
 
 /**
  * キーコードで指定されたキーが押されている状態をチェックする
  *
- * 押されたフレーム、離されたフレームは検出しない
- *
  * @param {Phaser.KeyCode} key_code
+ * @param {bool} 押された次のフレームから放される直前のフレームまでtrueを返す
  */
 App.Util.Input.Keyboard.prototype.isPressed = function(key_code)
 {
@@ -69,21 +70,20 @@ App.Util.Input.Keyboard.prototype.isPressed = function(key_code)
 	var idx = this.dobule_buffer_idx_;
 	return this.state_[key_code][idx]
 		&& this.state_[key_code][idx ^1];
-};
+}
 
 /**
  * キーコードで指定されたキーが押されているかををチェックする
  *
- * 押されたフレームから放されるまではtrueが返る
- *
  * @param {Phaser.KeyCode} key_code
+ * @return {bool} 押されたフレームから放される直前のフレームまでtrueが返る
  */
 App.Util.Input.Keyboard.prototype.isOn = function(key_code)
 {
 	this.ensurePresenceOfKey_(key_code);
 	var idx = this.dobule_buffer_idx_;
 	return this.state_[key_code][idx];
-};
+}
 
 /**
  * キーコードで指定されたキーが押されているかををチェックする
