cb49e6a6e9195c8d89625538c997933bd5e7b939
ike-jp <ike-jp@outlook.com>
2016-03-22 23:55:26 +0900

    Boot -> BootState

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@ Application.namespace('SuperILtan');
 
 var game = new Phaser.Game(256, 240, Phaser.AUTO, 'game');
 //var game = new Phaser.Game(256, 240, Phaser.CANVAS, 'game');
-game.state.add('Boot', SuperILtan.BootState);
+game.state.add('BootState', SuperILtan.BootState);
 game.state.add('ScenePreload', SuperILtan.PreloadState);
 game.state.add('SceneTitle', SuperILtan.TitleState);
 game.state.add('SceneLoad', SuperILtan.LoadingState);
 game.state.add('ScenePlay', SuperILtan.GameState);
 
-game.state.start('Boot');
+game.state.start('BootState');
