0850a36e14d854dc452215d51024f6f051971006
ike-jp <ike-jp@outlook.com>
2016-03-25 00:38:41 +0900

    プレハブ基底クラス追加

---
+++ js/prefabs/AbstractPrefab.js
@@ -0,0 +1,30 @@
+/**
+ * @author ike-jp <ikejpcw@gmail.com>
+ * @license Refer to the LICENSE file.
+ */
+
+Application.namespace('SuperILtan');
+
+/**
+ * プレハブの抽象クラス
+ *
+ * @constructor
+ */
+SuperILtan.AbstractPrehab = function(gameState, position, tiledMapObject) {
+	'use strict';
+	var textureName = tiledMapObject.properties.texture;
+	Phaser.Sprite.call(
+		this,
+		gameState.game,
+		position.x,
+		position.y,
+		textureName
+	);
+
+	var groupName = tiledMapObject.group;
+	this.gameState = gameState;
+	this.gameState.groups[groupName].add(this);
+}
+Application.inherits(SuperILtan.AbstractPrehab, Phaser.Sprite);
+
+
