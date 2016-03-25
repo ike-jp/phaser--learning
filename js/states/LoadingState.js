f032302a13c1c72f726a63d83157ec269cf8a3ab
ike-jp <ike-jp@outlook.com>
2016-03-26 01:25:37 +0900

    ロードを挟むように修正

--- js/states/LoadingState.js
+++ js/states/LoadingState.js
@@ -12,8 +12,8 @@ Application.namespace('SuperILtan.AbstractState');
  * @constructor
  * @extends {Scene}
  */
-SuperILtan.LoadingState = function()
-{
+SuperILtan.LoadingState = function() {
+	'use strict';
 	SuperILtan.AbstractState.call(this);
 }
 Application.inherits(SuperILtan.LoadingState, SuperILtan.AbstractState);
@@ -21,16 +21,26 @@ Application.inherits(SuperILtan.LoadingState, SuperILtan.AbstractState);
 /**
  * @override
  */
-SuperILtan.LoadingState.prototype.preload = function()
-{
+SuperILtan.LoadingState.prototype.preload = function() {
+	'use strict';
 	this.game.stage.backgroundColor = Phaser.Color.getColor(0, 0, 0);
+
+	// game
+	this.load.spritesheet('player', 'assets/objects/player.png', 16, 16);
+	this.load.spritesheet('enemies1', 'assets/objects/enemies1.png', 16, 16);
+	this.load.spritesheet('enemies2', 'assets/objects/enemies2.png', 16, 16);
+	this.load.spritesheet('items', 'assets/objects/items.png', 16, 16);
+	this.load.spritesheet('goal-symbol', 'assets/objects/goal-symbol.png', 32, 32);
+	this.load.image('bg', 'assets/tilemaps/tiles/bg.png');
+	this.load.image('terrain', 'assets/tilemaps/tiles/terrain.png');
+	this.load.tilemap('map', 'assets/tilemaps/json/level1.json', null, Phaser.Tilemap.TILED_JSON);
 }
 
 /**
  * @override
  */
-SuperILtan.LoadingState.prototype.create = function()
-{
+SuperILtan.LoadingState.prototype.create = function() {
+	'use strict';
 	this.game.time.events.add(
 		Phaser.Timer.SECOND * 1.5,
 		function() {
--- js/states/PreloadState.js
+++ js/states/PreloadState.js
@@ -25,15 +25,6 @@ SuperILtan.PreloadState.prototype.preload = function()
 	// title
 	this.load.image('title', 'assets/title.png');
 
-	// game
-	this.load.spritesheet('player', 'assets/objects/player.png', 16, 16);
-	this.load.spritesheet('enemies1', 'assets/objects/enemies1.png', 16, 16);
-	this.load.spritesheet('enemies2', 'assets/objects/enemies2.png', 16, 16);
-	this.load.spritesheet('items', 'assets/objects/items.png', 16, 16);
-	this.load.spritesheet('goal-symbol', 'assets/objects/goal-symbol.png', 32, 32);
-	this.load.image('bg', 'assets/tilemaps/tiles/bg.png');
-	this.load.image('terrain', 'assets/tilemaps/tiles/terrain.png');
-	this.load.tilemap('map', 'assets/tilemaps/json/level1.json', null, Phaser.Tilemap.TILED_JSON);
 	this.load.image('myfont', 'assets/fonts/myfont.png');
 	this.load.image('myfont-num-s', 'assets/fonts/myfont-num-s.png');
 }
@@ -44,5 +35,5 @@ SuperILtan.PreloadState.prototype.preload = function()
 SuperILtan.PreloadState.prototype.create = function()
 {
 	//this.state.start('SceneTitle');
-	this.state.start('ScenePlay');
+	this.state.start('SceneLoad');
 }
