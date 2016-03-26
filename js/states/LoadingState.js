d04c48677e8d8a4b107a7e0d3617be3eac95ba14
ike-jp <ike-jp@outlook.com>
2016-03-26 15:05:53 +0900

    レベルデータファイル名変更

--- js/states/LoadingState.js
+++ js/states/LoadingState.js
@@ -16,7 +16,10 @@ SuperILtan.LoadingState = function() {
 	'use strict';
 	SuperILtan.AbstractState.call(this);
 }
-Application.inherits(SuperILtan.LoadingState, SuperILtan.AbstractState);
+Application.inherits(
+	SuperILtan.LoadingState,
+	SuperILtan.AbstractState
+);
 
 /**
  * @override
@@ -24,7 +27,44 @@ Application.inherits(SuperILtan.LoadingState, SuperILtan.AbstractState);
 SuperILtan.LoadingState.prototype.preload = function() {
 	'use strict';
 	this.game.stage.backgroundColor = Phaser.Color.getColor(0, 0, 0);
+	this.load.tilemap('map', 'assets/tilemaps/json/level1_map.json', null, Phaser.Tilemap.TILED_JSON);
 
+	this.load.text('level', 'assets/tilemaps/json/level.json');
+	var levelText = this.game.cache.getText('level');
+console.log(levelData);
+	var levelData = JSON.parse(levelText);
+/*
+	var assets = levelData.assets;
+	for (asset_key in assets) {
+		if (assets.hasOwnProperty(asset_kye)) {
+			asset = assets[asset_key];
+			switch (asset.type) {
+				case 'image':
+					this.load.image(asset_key, asset.source);
+					break;
+				case 'spritesheet':
+					this.load.image(
+						asset_key,
+						asset.source,
+						asset.frame_width,
+						asset.frame_height,
+						asset.frames,
+						asset.margin,
+						asset.spacing
+					);
+					break;
+				case 'tilemap':
+					this.load.tilemap(
+						asset_key,
+						asset.source,
+						null,
+						Phaser.Tilemap.TILED_JSON
+					);
+					break;
+			}
+		}
+	}
+*/
 	// game
 	this.load.spritesheet('player', 'assets/objects/player.png', 16, 16);
 	this.load.spritesheet('enemies1', 'assets/objects/enemies1.png', 16, 16);
@@ -33,7 +73,6 @@ SuperILtan.LoadingState.prototype.preload = function() {
 	this.load.spritesheet('goal-symbol', 'assets/objects/goal-symbol.png', 32, 32);
 	this.load.image('bg', 'assets/tilemaps/tiles/bg.png');
 	this.load.image('terrain', 'assets/tilemaps/tiles/terrain.png');
-	this.load.tilemap('map', 'assets/tilemaps/json/level1.json', null, Phaser.Tilemap.TILED_JSON);
 }
 
 /**
